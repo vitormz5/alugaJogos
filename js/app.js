b129e9265f2c2306b615c693474752ae8ae41f65
Vitor Maiochi Ziehlsdorff <maiochivitor@gmail.com>
2024-01-29 21:10:34 -0300

    "código documentado"

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// Troca o h1 e confirma se alugou / devolveu o jogo
 function trocaTexto(tag, texto) {
   let notifica = document.querySelector(tag);
   notifica.innerHTML = texto;
@@ -13,12 +14,16 @@ function alterarStatus(id) {
 
   // Devolve a lista de classe do elemento, e verifica se contém a classe dashboard__item__img--rented //
   if (imagem.classList.contains("dashboard__item__img--rented")) {
+    // Se tiver, remove a classe dashboard__item__img--rented
     imagem.classList.remove("dashboard__item__img--rented");
+    // Verifica se o botão contém a classe dashboard__item__button--return
     botao.classList.contains("dashboard__item__button--return");
+    // Se tiver, remove a classe dashboard__item__img--rented
     botao.classList.remove("dashboard__item__button--return");
     botao.innerHTML = "Alugar";
     trocaTexto("h1", "Você devolveu o jogo com sucesso!");
   } else {
+    // Se não tiver classe dashboard__item__img--rented, adiciona ela ao elemento (Aluga o jogo)
     imagem.classList.add("dashboard__item__img--rented");
     botao.classList.add("dashboard__item__button--return");
     botao.innerHTML = "Devolver";
